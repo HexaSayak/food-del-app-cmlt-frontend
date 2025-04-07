62f077bf672fc15da2b24b271c91482cc50dbaaa
HexaSayak <sayakghosh24012001@gmail.com>
2025-04-07 13:55:54 +0530

    complete front end commit

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -1,24 +1,35 @@
 import { createContext, useEffect, useState } from "react";
-import { food_list } from "../assets/assets";
-
+// import { food_list } from "../assets/assets";
+import axios from 'axios';
 export const StoreContext = createContext(null)
 
 const StoreContextProvider = (props) => {
 
 
     const [cartItems, setCartItems] = useState({});
+    // const url ="http://localhost:4000"
+    const url =" https://food-del-app-backend-x861.onrender.com"
+    const [token,setToken] = useState("")
 
-    const addToCart = (itemId) => {
+    const [food_list,setFoodList] = useState([])
+
+    const addToCart = async (itemId) => {
         if (!cartItems[itemId]) {
             setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
         }
         else {
             setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
         }
+        if (token) {
+            await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})
+        }
     }
 
-    const removeFromCart = (itemId) => {
+    const removeFromCart = async (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
+        if (token) {
+            await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}})
+        }
     }
 
     const getTotalCartAmount = () => {
@@ -31,6 +42,27 @@ const StoreContextProvider = (props) => {
         }
         return totalAmount;
     }
+    
+    const fetchFoodList = async () => {
+        const response = await axios.get(url+"/api/food/list");
+        setFoodList(response.data.data)
+    }
+    
+    const loadCartData = async (token) => {
+        const response = await axios.post(url+"/api/cart/get",{},{headers:{token}});
+        setCartItems(response.data.cartData);
+    }
+    useEffect(()=>{
+        
+        async function loadData() {
+            await fetchFoodList();
+            if(localStorage.getItem("token")){
+                setToken(localStorage.getItem("token"));
+                await loadCartData(localStorage.getItem("token"))
+            }
+        }
+        loadData();
+    },[])
 
     // useEffect(()=>{
     //     console.log(cartItems);
@@ -42,7 +74,10 @@ const StoreContextProvider = (props) => {
         setCartItems,
         addToCart,
         removeFromCart,
-        getTotalCartAmount
+        getTotalCartAmount,
+        url,
+        token,
+        setToken
     }
 
     return (
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
 
-  const { cartItems, food_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount,url } = useContext(StoreContext);
   const navigate = useNavigate();
 
   return (
@@ -22,11 +22,11 @@ const Cart = () => {
         <br />
         <hr />
         {food_list.map((item, index) => {
-          if (cartItems[item._id] > 0) {
+          if (cartItems[item._id]) {
             return (
-              <div>
+              <div key={item._id}>
                 <div className='cart-items-title cart-items-item'>
-                  <img src={item.image} alt="" />
+                  <img src={url+"/images/"+item.image} alt="" />
                   <p>{item.name}</p>
                   <p>₹{item.price}</p>
                   <p>{cartItems[item._id]}</p>
@@ -37,6 +37,7 @@ const Cart = () => {
               </div>
             )
           }
+          return null;
         })}
       </div>
       <div className="cart-botton">
